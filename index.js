e924e9e3abf227cbddd8605c9b6c8952dd868291
kkortes <kim@kortes.se>
2021-03-22 23:10:49 +0100

    feature: prepared to be a package on npm

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
-const request = (name, args) =>
-  new Promise((resolve, reject) => {
-    const id = setTimeout(
-      () => reject({ error: "Conduit client error: Request timed out" }),
-      1000
+import fs from "fs";
+import { Server } from "socket.io";
+
+const serveEndpoints = (io, socket, extra, path) =>
+  fs.readdirSync(`${process.cwd()}/${path}`).forEach(async directory => {
+    const { default: defaultExport } = await import(
+      `${process.cwd()}/${path}/${directory}`
     );
-    socket.emit(name, args, response => {
-      clearTimeout(id);
-      if (response.hasOwnProperty("error")) {
-        reject(response);
-      } else {
-        resolve(response);
-      }
-    });
+    socket.on(
+      directory.replace(".js", ""),
+      defaultExport.constructor.name === "AsyncFunction"
+        ? async (body, callback) => {
+            let result;
+            try {
+              result = await defaultExport(body, io, socket, extra);
+            } catch (error) {
+              result = { error: `Conduit server error: ${error.toString()}` };
+            }
+            return callback(result);
+          }
+        : (body, callback) =>
+            callback
+              ? callback({
+                  error: `Conduit server error: the function you called isn't asyncronous`,
+                })
+              : defaultExport(body, io, socket, extra)
+    );
   });
+
+const Conduit = (
+  path,
+  hooks = {},
+  port = 1337,
+  config = {
+    cors: {
+      origin: "*",
+    },
+  }
+) => {
+  if (!path) throw new Error("Path must be set");
+  const io = new Server(port, config);
+  io.on("connection", socket => serveEndpoints(io, socket, hooks, path));
+  console.log(`Server started on port ${port}`);
+  return io;
+};
+
+export default Conduit;
