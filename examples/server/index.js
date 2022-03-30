5dde72450c4a6eb8f789f34ff4ff623793bf4094
kkortes <kim@kortes.se>
2022-03-30 21:45:10 +0200

    fix

--- examples/server/index.js
+++ examples/server/index.js
@@ -4,22 +4,6 @@ dotenv.config();
 
 const { PORT, CORS_ORIGIN } = process.env;
 
-const typeSizes = {
-  undefined: () => 0,
-  boolean: () => 4,
-  number: () => 8,
-  string: (item) => 2 * item.length,
-  object: (item) =>
-    !item
-      ? 0
-      : Object.keys(item).reduce(
-          (total, key) => sizeOf(key) + sizeOf(item[key]) + total,
-          0
-        ),
-};
-
-const sizeOf = (value) => typeSizes[typeof value](value);
-
 aaw(
   "events",
   {},
@@ -33,7 +17,6 @@ aaw(
     toLog.push(version || "n/a");
     toLog.push(accountName || socketID);
     if (error) toLog.push(error);
-    toLog.push(sizeOf(body));
 
     log(toLog.join(" | "));
   }
